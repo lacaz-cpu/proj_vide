65509e6e4bbe9c4d55997c51a7cb3425319c7e87
lacaz-cpu <arthur.obry.2n16@gmail.com>
2025-05-11 12:59:56 +0200

    flo

--- src/moteur_jeu.c
+++ src/moteur_jeu.c
@@ -4,36 +4,38 @@
 #include "raylib.h"
 
 
-#define COLS 3
-#define ROWS 3
+#define COLS 72
+#define ROWS 72
 
-const int screenWidth = 300;
-const int screenHeight = 300;
+const int screenWidth = 900;
+const int screenHeight = 900;
 
-const int tileWidth = 100;
-const int tileHeight = 100;
+const int tilesize = 100;
 
 tuile tile_num[24];
 tuile Pile[72];
 Texture2D textures[24]={};
 
 void tileDraw(tuile t,int x,int y){
-    DrawTexture(textures[t.numero],x*tileWidth,y*tileHeight,WHITE);
+    DrawTexture(textures[t.numero],x*tilesize,y*tilesize,WHITE);
 }
 
 int main(void){
     parseur_csv("./src/tuile_liste.csv",tile_num);
     parseur_csv("./src/tuiles_base_simplifiees.csv",Pile);
     find_num(Pile,tile_num);
-    Camera2D camera={0};
-    camera.target=(Vector2){0,0};   // Point que la caméra suit
-    camera.offset=(Vector2){150,150}; // Centre de l'écran
+    Camera2D camera = { 0 };
+    camera.target = (Vector2){71*tilesize,71*tilesize};
+    camera.offset = (Vector2){screenHeight/2,screenWidth/2};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
     InitWindow(screenWidth, screenHeight, "Carcassone");
     char tmp[30];
     for(int i=0; i<24; i++){
         sprintf(tmp,"./src/tile/Tile_%d.png",i);
         textures[i]=LoadTexture(tmp);
     }
+    int x,y;
     tuile tr = piocher(Pile,0);
     SetTargetFPS(60);
     while (!WindowShouldClose()){
@@ -41,14 +43,30 @@ int main(void){
         if (IsKeyDown(KEY_LEFT)) camera.target.x-=5;
         if (IsKeyDown(KEY_UP)) camera.target.y-=5;
         if (IsKeyDown(KEY_DOWN)) camera.target.y+=5;
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+            Vector2 mPos = GetScreenToWorld2D(GetMousePosition(),camera);
+            int indexI = mPos.x / tilesize;
+            int indexJ = mPos.y / tilesize;
+            if(indexI >= 0  && indexI < COLS*2 && indexJ >= 0 && indexJ < ROWS*2){
+                x = indexI;
+                y = indexJ;
+            }
+        }
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        for(int i = 0; i < 72;i++){
-            for(int j = 0; j < 72;j++){
-                DrawRectangleLines(i*tileWidth,j*tileHeight,tileWidth,tileHeight,BLACK);
+        BeginMode2D(camera);
+        Vector2 mx = GetMousePosition();
+        int I = mx.x / tilesize;
+        int J = mx.y / tilesize;
+        DrawText(TextFormat("[%i, %i]", I, J),150,150,0,BLACK);
+        for(int i = 0; i < ROWS*2;i++){
+            for(int j = 0; j < COLS*2;j++){
+                DrawRectangleLines(i*tilesize,j*tilesize,tilesize,tilesize,BLACK);
             } 
         }
-        tileDraw(tr,1,1);
+        tileDraw(tr,71,71);
+        EndMode2D();
+
         //tileDraw(piocher(Pile,1),1,2);
         EndDrawing();
     }
